14ad4f6a045d3bd838e520b59faf0778eea1cc8f
todd <todd+github@goneopen.com>
2025-02-06 12:04:36 +1300

    upgrade

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,14 +4,14 @@ import faq from '../../../components/faq.vue'
 import explanation from '../../../components/explanation.vue'
 import prompt from '../../../components/prompt.vue'
 import instructions from '../../../components/instructions.vue';
+import { Theme } from 'vitepress';
 
-
-DefaultTheme.enhanceApp = ({ app }) => {
-    app.component('faq', faq)
-    app.component('explanation', explanation)
-    app.component('prompt', prompt)
-    app.component('instructions', instructions)
-}
-
-export default DefaultTheme
-
+export default {
+    extends: DefaultTheme,
+    enhanceApp({ app }) {
+        app.component('faq', faq)
+        app.component('explanation', explanation)
+        app.component('prompt', prompt)
+        app.component('instructions', instructions)
+    },
+} satisfies Theme;
